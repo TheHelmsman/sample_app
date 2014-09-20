865298ea2faa5b601436c8223296fd96107e6b00
The Helmsman <helmsman@gmail.com>
2014-09-20 13:23:10 +0300

    Excersises 5.6 - done; Sorter tests for static pages; Add link tests; full_title method put in application helper;

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,16 +6,25 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1', text: 'Sample App') }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+    it_should_behave_like "all static pages"
+
+    #it { should have_selector('h1', text: 'Sample App') }
     #it "should have the h1 'Sample App'" do
       #visit '/static_pages/home'
       #page.should have_selector('h1', text: 'Sample App')
     #end
 
-    it { should have_selector('title', text: full_title('')) }
+    #it { should have_selector('title', text: full_title('')) }
     #it "should have the title" do
       #visit '/static_pages/home'
       #page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App")
@@ -26,27 +35,52 @@ describe "StaticPages" do
       #visit '/static_pages/home'
       #page.should_not have_selector('title', text: '| Home')
     #end
+
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'title', text: full_title('About Us')
+      click_link "Help"
+      page.should have_selector 'title', text: full_title('Help')
+      click_link "Contact"
+      page.should have_selector 'title', text: full_title('Contact')
+      click_link "Home"
+      click_link "Sign up now!"
+      page.should have_selector 'title', text: full_title('Sign up')
+      click_link "sample app"
+      page.should have_selector 'title', text: full_title('')
+    end
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+    it_should_behave_like "all static pages"
+
+    #it { should have_selector('h1', text: 'Help') }
+    #it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
+    it_should_behave_like "all static pages"
+    #it { should have_selector('h1', text: 'About Us') }
+    #it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "all static pages"
+    #it { should have_selector('h1', text: 'Contact') }
+    #it { should have_selector('title', text: full_title('Contact')) }
   end
 
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,13 @@
 # files in spec/support directory automatically included by RSpec
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+
+include ApplicationHelper
+
+# the following code replaced by application helper
+#def full_title(page_title)
+  #base_title = "Ruby on Rails Tutorial Sample App"
+  #if page_title.empty?
+  #  base_title
+  #else
+  #  "#{base_title} | #{page_title}"
+  #end
+#end
