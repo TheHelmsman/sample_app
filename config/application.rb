44ecc83b2143db5f09f97cc5378042729ea95ef9
Igor Vasiliev <igor.vasiliev@dramaforum.fi>
2015-01-22 13:03:38 +0200

    Adds workaround for running rspec tests with Ruby 2.0 (originally project is written for Ruby 1.9.3 and Rails 3.2.3

--- config/application.rb
+++ config/application.rb
@@ -63,3 +63,22 @@ module SampleApp
     config.assets.version = '1.0'
   end
 end
+
+# Because of the Ruby 2.0 privatizes the method and  Rails 3.2.12's use of it
+# , and because Rails 3.2.13 (which has a fix) also could potentially
+# introduce other bugs, we're adding this patch to fix the issue.
+#
+# Remove this if the project contains Rails >= 3.2.13
+module ActiveModel
+  class Errors
+    public :initialize_dup
+  end
+
+  module Validations
+    public :initialize_dup
+  end
+end
+
+class ActiveRecord::Base
+  public :initialize_dup
+end
